c6cd84b0abe240b55ecd953c00c33e8f5ff8071e
cutrain <454615501@qq.com>
2017-11-27 16:02:51 +0800

    feat: regular the json format

--- algorithm/src/api.cc
+++ algorithm/src/api.cc
@@ -6,28 +6,32 @@
 #include <json/json.h>
 
 
-const char* print_message(int message_code, const char append_message[]);
-
-char retstr[100];
-const char* print_message(int message_code, const char append_message[]="") {
-	// TODO: map code to message & use recycle memory
-	sprintf(retstr, "%d / %s", message_code, append_message);
-	return retstr;
+const char* make_json(const char* func_name, int status_code, Json::Value&& json = Json::Value()) {
+	json["status"] = status_code;
+	json["function_name"] = func_name;
+	string str = json.toStyledString();
+	int len = str.length();
+	static char* s;
+	delete[] s;
+	s = new char(1 + len);
+	memcpy(s, str.c_str(), len);
+	return s;
 }
+
 
 const char* Create(char* name, char* type, char* param) {
 	string sname = string(name);
 	string stype = string(type);
 	string sparam = string(param);
 	int ret = graphManager.Create(sname, stype, sparam);
-	return print_message(ret, "create");
+	return make_json("create", ret);
 }
 
 const char* Delete(char* name) {
 	// TODO: delete related edge
 	string sname = string(name);
 	int ret = graphManager.Delete(sname);
-	return print_message(ret, "delete");
+	return make_json("delete", ret);
 }
 
 const char* Connect(char* out_node, char* out_port, char* in_node, char* in_port) {
@@ -36,7 +40,7 @@ const char* Connect(char* out_node, char* out_port, char* in_node, char* in_port
 	string sin_node = string(in_node);
 	string sin_port = string(in_port);
 	int ret = graphManager.Connect(sout_node, sout_port, sin_node, sin_port);
-	return print_message(ret, "connect");
+	return make_json("connect", ret);
 }
 
 const char* Disconnect(char* out_node, char* out_port, char* in_node, char* in_port) {
@@ -45,7 +49,7 @@ const char* Disconnect(char* out_node, char* out_port, char* in_node, char* in_p
 	string sin_node = string(in_node);
 	string sin_port = string(in_port);
 	int ret = graphManager.Disconnect(sout_node, sout_port, sin_node, sin_port);
-	return print_message(ret, "disconnect");
+	return make_json("disconnect", ret);
 }
 
 const char* GetValue(char* name, char* out_port, int start_row, int end_row) {
@@ -53,9 +57,9 @@ const char* GetValue(char* name, char* out_port, int start_row, int end_row) {
 	string sname = string(name);
 	string sout_port = string(out_port);
 	if (graphManager.CheckName(sname) == NAME_NOT_FOUND)
-		return print_message(NAME_NOT_FOUND, "getvalue");
+		return make_json("getvalue", NAME_NOT_FOUND);
 	if (start_row < -1 || end_row < -1)
-		return print_message(RANGE_EXCCED, "getvalue");
+		return make_json("getvalue", RANGE_EXCCED);
 	print("API::GetValue 1\n");
 
 	NodePtr node = graphManager.GetNode(sname);
@@ -64,7 +68,7 @@ const char* GetValue(char* name, char* out_port, int start_row, int end_row) {
 	print("API::GetValue 3\n");
 	// TODO:return value with json
 	if (start_row >= value->dim[0] || end_row >= value->dim[0])
-		return print_message(RANGE_EXCCED, "getvalue");
+		return make_json("getvalue", RANGE_EXCCED);
 	if (start_row == -1)
 	{
 		start_row = 0;
@@ -88,19 +92,14 @@ const char* GetValue(char* name, char* out_port, int start_row, int end_row) {
 	}
 	print("API::GetValue Leave\n");
 
-	// TODO: change it into a recycleable memory allocate
-	// NOTE: These code will cause memory leak, please attention
-	string s = json.toStyledString();
-	char* ret = new char[s.length()+1];
-	memcpy(ret, s.c_str(), s.length());
-	return ret;
+	return make_json("getvalue", OK, std::move(json));
 }
 
 const char* Run(char* name) {
 	print("api::Run Enter\n");
 	string sname = string(name);
 	if (graphManager.CheckName(sname) == NAME_NOT_FOUND)
-		return print_message(NAME_NOT_FOUND, "run");
+		return make_json("run", NAME_NOT_FOUND);
 	print("api::Run 1\n");
 	DataPtrMap datas = graphManager.GetInput(sname);
 	print("api::Run 2\n");
@@ -108,16 +107,16 @@ const char* Run(char* name) {
 	print("api::Run 3\n");
 	int ret = node->Run(datas);
 	print("api::Run Leave\n");
-	return print_message(ret, "run");
+	return make_json("run", ret);
 }
 
 const char* RunTo(char* name) {
 	// TODO
-	return "ERROR occurred without define";
+	return make_json("runto", NOT_DEFINED);
 }
 
 const char* RunAll() {
 	// TODO
-	return "ERROR occurred without define";
+	return make_json("runall", NOT_DEFINED);
 }
 
